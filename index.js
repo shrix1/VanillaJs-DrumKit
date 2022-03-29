adc4dc215c79b3d1a0fc11cc0d80576dc4e023f4
Shriprasanna B <shriprasanna32@gmail.com>
2022-03-29 21:46:20 +0530

    updated

---
+++ index.js
@@ -0,0 +1,20 @@
+//event
+window.addEventListener("keydown", play);
+
+//function
+function play(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const btn = document.querySelector(`button[data-key="${e.keyCode}"]`);
+  if (!audio) {
+    return;
+  } else {
+    audio.currentTime = 0;
+    audio.play();
+    btn.classList.add("pressed");
+    
+    //(remove pressed class its a dump way) but i learned setInterval func
+    setInterval(() => {
+      btn.classList.remove("pressed");
+    }, 500);
+  }
+}
