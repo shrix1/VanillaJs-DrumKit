6ad7ad7f482da1e688e629be6e3232930d333b23
Shriprasanna B <shriprasanna32@gmail.com>
2022-09-04 09:48:24 +0530

    fixed issue

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 window.addEventListener("keydown", play);
 
 //function
-const play=(e)=> {
+function play(e) {
   const key = e.keyCode; //keycode
 
   //getting keyCode to play specfic audio
   const audio = document.querySelector(`audio[data-key="${key}"]`);
-  
+
   //getting btn clicks
   const btn = document.querySelector(`button[data-key="${key}"]`);
 
@@ -17,10 +17,10 @@ const play=(e)=> {
     audio.currentTime = 0;
     audio.play();
     btn.classList.add("pressed");
-    
+
     //(remove pressed class using setInterval is a dump way) but i learned setInterval func
     setInterval(() => {
       btn.classList.remove("pressed");
-    }, 400);
+    }, 100);
   }
 }
